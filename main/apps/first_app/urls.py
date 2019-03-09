e77715fc36fa1854fedb6377db3638074ebbc852
taylor-brudos <taylor.brudos@gmail.com>
2019-03-09 12:57:35 -0800

    html updates

--- main/apps/first_app/urls.py
+++ main/apps/first_app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^myaccounts$', views.myaccounts),
+    url(r'^accountdetail$', views.show),
 ]
--- main/apps/first_app/views.py
+++ main/apps/first_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    context = {
-        "email" : "blog@gmail.com",
-        "name" : "mike"
-    }
-    return render(request, "first_app/index.html", context)
+    return render(request, "first_app/index.html")
+
+def myaccounts(request):
+    return render(request, "first_app/my_accounts.html")
     
+def show(request):
+    return render(request, "first_app/account_detail.html")
