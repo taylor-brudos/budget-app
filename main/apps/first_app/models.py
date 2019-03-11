0cabbb9359b90e8390fce6d6df56e497d9267bcf
taylor-brudos <taylor.brudos@gmail.com>
2019-03-10 17:17:21 -0700

    added in models

--- main/apps/first_app/models.py
+++ main/apps/first_app/models.py
@@ -1,3 +1,38 @@
+from __future__ import unicode_literals
 from django.db import models
+# import bcrypt
+import re
+import datetime
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    # user_level = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    # objects = UserManager()
+
+class Account(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    user = models.ForeignKey(User, related_name = "accounts")
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class Transaction(models.Model):
+    trans_date = models.DateField()
+    trans_type = models.CharField(max_length=255)
+    amount = models.FloatField()
+    notes = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    category = models.ForeignKey(Category, related_name = "transactions")
+
